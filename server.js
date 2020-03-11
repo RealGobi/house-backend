b7cde82878f45d30c7dd83bbdd6da05720a28dc2
Jimmy Svensson <jimmy888swe@gmail.com>
2020-03-11 23:25:32 +0100

    feat: connection to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyPaser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 4000;
@@ -8,6 +9,17 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyPaser.json());
 
+// Connect to Mongo
+
+mongoose
+.connect('mongodb+srv://jimmy:' + process.env.db_pass + '@house-app-db-g49yq.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
 
 app.listen(PORT, function() {
   console.log("Server is running on port: ", PORT);
