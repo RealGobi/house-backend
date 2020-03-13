ede4b31e1cc2ec245f7f47461320d5c1bdc4a83d
Jimmy Svensson <jimmy888swe@gmail.com>
2020-03-13 22:25:02 +0100

    feat: post route for task

--- routes/task.js
+++ routes/task.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 // Task Model
 const Task = require('../models/task-model');
@@ -18,6 +18,21 @@ router.get('/:id', (req, res) => {
   let id = req.params.id;
   Task.findById(id)
   .then(task => res.json(task));
+});
+
+// @route   POST /Task
+// @desc    Create An Task
+// @access  All
+
+router.post('/', (req, res) => {
+  const newTask = new Task({
+    _id: new mongoose.Types.ObjectId(),
+    title: req.body.title,
+    step: req.body.step,
+    description: req.body.description,
+  });
+
+  newTask.save().then(task => res.json(task));
 });
 
 module.exports = router;
