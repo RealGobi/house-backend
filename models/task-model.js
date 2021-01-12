dfeda0f59c936ac42a020b718d57790b20eddd80
Jimmy Svensson <jimmy888swe@gmail.com>
2021-01-12 23:54:08 +0100

    fix: remode unused code in model and commented some out

--- models/task-model.js
+++ models/task-model.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const taskSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     title: { type: String, required: true },
-    step: { type: String, required: true },
-    description: { type: String, required: true },
-    open: { type: Boolean, required: true },
+/*     step: { type: String, required: true }, */ 
+   description: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Task', taskSchema);
