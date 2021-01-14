e1c4f6a3d0b8932c172e6b6ddeaaeb6f825752ea
Jimmy Svensson <jimmy888swe@gmail.com>
2021-01-15 00:32:29 +0100

    feat: auth to protect routes

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+function auth(req,res,next) {
+  const token = req.header('x-auth-token');
+
+  //check for token
+  if(!token) res.status(401).json({msg: 'Medges ej!, ingen token för inloggning'});
+
+  try {
+      // valitate token 
+      const decode =  jwt.verify(token, config.get('jwt_key'));
+    
+      //add user from payload
+      req.user = decode;
+      next();
+    
+  } catch (error) {
+    res.status(400).json({msg: 'Inloggning misslyckades'})
+  }
+};
+
+module.exports = auth;
