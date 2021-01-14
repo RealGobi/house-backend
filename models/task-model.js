ce805df69f80691e328e5ec9287ecc59adb35109
Jimmy Svensson <jimmy888swe@gmail.com>
2021-01-14 13:16:55 +0100

    fix: added date to model

--- models/task-model.js
+++ models/task-model.js
@@ -4,7 +4,8 @@ const taskSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     title: { type: String, required: true },
 /*     step: { type: String, required: true }, */ 
-   description: { type: String, required: true }
+   description: { type: String, required: true },
+   reg_date: { type: Date, default:Date.now }
 });
 
 module.exports = mongoose.model('Task', taskSchema);
