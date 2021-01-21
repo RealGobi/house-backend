74232ecf31af9af1e40e8bf18be4a2118fe68c8c
Jimmy Svensson <jimmy888swe@gmail.com>
2021-01-21 14:35:35 +0100

    fix: add return statment to if check

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ function auth(req,res,next) {
   const token = req.header('x-auth-token');
 
   //check for token
-  if(!token) res.status(401).json({msg: 'Medges ej!, ingen token för inloggning'});
+  if(!token) return res.status(401).json({msg: 'Medges ej!, ingen token för inloggning'});
 
   try {
       // valitate token 
