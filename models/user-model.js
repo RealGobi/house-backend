7cb2633e5918ec1eabf569c6c8af51ec653b3860
Jimmy Svensson <jimmy888swe@gmail.com>
2021-01-25 00:02:15 +0100

    fix: add street to user

--- models/user-model.js
+++ models/user-model.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true },
-   email: { type: String, required: true, unique:true },
-   password: { type: String, required: true },
-   reg_date: { type: Date, default:Date.now }
+    email: { type: String, required: true, unique:true },
+    password: { type: String, required: true },
+    street: { type: String, required: true },
+    reg_date: { type: Date, default:Date.now }
 });
 
 module.exports = mongoose.model('user', userSchema);
